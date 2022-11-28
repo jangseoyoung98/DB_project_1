1f9a13502e2407e83b4afc6e20410efbbaeaf538
jangseoyoung98 <jangseoyoung98@gmail.com>
2022-11-28 17:41:15 +0900

    complete 1st

--- menu/views.py
+++ menu/views.py
@@ -10,7 +10,7 @@ def home_view(request):
         categorys = CategoryForm.MenuCategory.names_c
         # allergys = CategoryForm.Allergy.names_a
 
-        return render(request, "home.html", {"brands": brands, "categorys": categorys, "allergys": allergys})
+        return render(request, "home.html", {"brands": brands, "categorys": categorys})
 
     if request.method == 'POST':
         listings = Menu.objects.all()
@@ -20,15 +20,14 @@ def home_view(request):
         # allergys = CategoryForm.Allergy.names_a
 
         searched = request.POST
-#        calorie = searched["calorie"]
+        calorie = searched["calorie"]
         price = searched["money"]
-#        caffeine = searched["caffeine"]
+        caffeine = searched["caffeine"]
         brand_id = searched["brands"]
         category_id = searched["categorys"]
         # allergy_id = searched["allergys"]
 
-        # allergy 수정 - 이 id에 포함된 메뉴_id를 파악해서 그 음료들은 exclude
-        queryset_1 = Menu.objects.filter(brand_id=brand_id, category_id=category_id, price__lt=price)
+        queryset_1 = Menu.objects.filter(brand_id=brand_id, category_id=category_id, price__lt=price, calorie__lt=calorie, caffeine__lt=caffeine)
         print(queryset_1)
 
         context = {
@@ -41,30 +40,3 @@ def home_view(request):
         }
 
         return render(request, 'home.html', context)
-
-"""
-def home_view(request):
-    listings = Listing.objects.all()
-    listing_filter = ProductFilter(request.GET, queryset=listings)
-    context = {
-        'listing_filter': listing_filter
-    }
-    return render(request, 'templates/home.html', context)
-
-def list_view(request):
-    if request.method == 'GET':
-        listing_form = ListingForm()
-        return render(request, 'templates/list.html', {'listing_form': listing_form})
-
-def listing_view(request, id):
-    try:
-        listing = Listing.objects.get(id=id)
-        if listing is None:
-            raise Exception
-        return render(request, 'views/listing.html', {'listing': listing, })
-    except Exception as e:
-        messages.error(request, f'Invalid UID {id} was provided for listing.')
-        return redirect('home')
-
-
-"""
